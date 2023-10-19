5dc6f28e54002b1ed35bb636d33cdef23492292c
migutierrez6 <ivan291194@gmail.com>
2023-10-19 14:35:48 -0600

    set debug mode to true change it to home page

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
-
+app.debug = True
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "<h1>Home page</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
