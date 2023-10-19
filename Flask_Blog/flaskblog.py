4849ef7b8da9115ebcbea6eddaa37bf3fb00af5b
migutierrez6 <ivan291194@gmail.com>
2023-10-19 14:40:14 -0600

    created the about page

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -5,5 +5,9 @@ app.debug = True
 def hello():
     return "<h1>Home page</h1>"
 
+@app.route("/about")
+def about():
+    return "<h1>about page</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
