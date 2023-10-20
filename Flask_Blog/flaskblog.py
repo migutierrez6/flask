27fbefc6654924e62b05591df5122697a6ff7b7e
ivan <ivan291194@gmail.com>
2023-10-19 20:10:49 -0600

    Added about and home html files Added posts to home page added title to the home page and about page

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
+
+posts = [
+    {
+        'author': 'Ivan Gutierrez',
+        'title': 'Flask project 1',
+        'content': 'first post content',
+        'date_posted': 'october 19 2023'
+    },
+    {
+        'author': 'Manuel Flores',
+        'title': 'Flask project 2',
+        'content': 'second post content',
+        'date_posted': 'october 19 2023'
+    }
+]
 @app.route("/")
-def hello():
-    return "<h1>Home page</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>about page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
